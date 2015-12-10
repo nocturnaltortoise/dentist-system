b622de1389be4cc91fa567cf670e7666dadd7c9d
Andreas Theocharous <atheocharous2@sheffield.ac.uk>
2015-12-10 17:53:58 +0000

    The code that created the tables

---
+++ src/src/CreateTables.java
@@ -0,0 +1,96 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class createTables
+{
+        public static void main (String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException
+        {
+                Connection Conn = null;
+                Statement stmt = null;
+                Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+                String DB="jdbc:mysql://stusql.dcs.shef.ac.uk/team001?user=team001&password=55e68e81";
+                try
+                {
+                        Conn = DriverManager.getConnection(DB);
+                        stmt = Conn.createStatement(); 
+                        
+                        String sql = "CREATE TABLE Address " +
+                        	"(AddressID INTEGER AUTO_INCREMENT, " +
+                        	" HouseNum INTEGER NOT NULL, " +
+                        	" Street VARCHAR(50) NOT NULL, " +
+                        	" District VARCHAR(50), " +
+                        	" City VARCHAR(50) NOT NULL, " +
+                        	" Postcode VARCHAR(15) NOT NULL, " +
+                        	" PRIMARY KEY ( AddressID ))";
+                        
+                        stmt.executeUpdate(sql);
+                        
+                        
+                        sql = "CREATE TABLE Plan " +
+                        	"(Name VARCHAR(30) UNIQUE NOT NULL, " +
+                            " MonthlyP INTEGER NOT NULL, " +
+                            " Level VARCHAR(100) NOT NULL, " +
+                            " PRIMARY KEY ( Name ))";
+                                
+                        stmt.executeUpdate(sql);
+                     
+                        
+                        sql = "CREATE TABLE Patient " +
+                        	"(PatientID INTEGER AUTO_INCREMENT, " +
+                            " Title VARCHAR(10), " +
+                            " First VARCHAR(50) NOT NULL, " +
+                            " Last VARCHAR(50) NOT NULL, " +
+                            " Dob DATE, " +
+                            " Tel VARCHAR(20), " +
+                            " AddressID INTEGER NOT NULL, " +
+                            " PlanName VARCHAR(30), " +
+                            " PRIMARY KEY ( PatientID ), " +
+                            " FOREIGN KEY (AddressID) REFERENCES Address(AddressID), " +
+                            " FOREIGN KEY (PlanName) REFERENCES Plan(Name))";
+                             
+                        stmt.executeUpdate(sql);
+                        
+                        
+                        sql = "CREATE TABLE TreatmentType " +
+                            "(Name VARCHAR(30) UNIQUE NOT NULL, " +
+                            " Cost INTEGER NOT NULL, " +
+                            " PRIMARY KEY ( Name ))";
+                                    
+                        stmt.executeUpdate(sql);
+                        
+                        
+                        sql = "CREATE TABLE Appointment " +
+                            "(AppointmentID INTEGER AUTO_INCREMENT, " +
+                            " Date DATE NOT NULL, " +
+                            " StartTime TIME NOT NULL, " +
+                            " EndTime TIME NOT NULL, " +
+                            " Partner VARCHAR(10) NOT NULL, " +
+                            " PatientID INTEGER NOT NULL, " +
+                            " PRIMARY KEY ( AppointmentID ), " +
+                            " FOREIGN KEY (PatientID) REFERENCES Patient(PatientID))";
+                        
+                        stmt.executeUpdate(sql);
+                        
+                        
+                        sql = "CREATE TABLE Treatment " +
+                            "(AppointmentID INTEGER NOT NULL, " +
+                            " TreatmentName VARCHAR(30) NOT NULL, " +
+                            " PRIMARY KEY ( AppointmentID, TreatmentName ), " +
+                            " FOREIGN KEY (AppointmentID) REFERENCES Appointment(AppointmentID), " +
+                            " FOREIGN KEY (TreatmentName) REFERENCES TreatmentType(Name))";
+                            
+                        stmt.executeUpdate(sql);
+                }
+                catch (SQLException e)
+                {
+                        e.printStackTrace();
+                }
+                finally
+                {
+                        if (Conn != null) Conn.close();
+                }
+        }
+}
