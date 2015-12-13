b2201db8e3eed9df065821a0600b05df54b3d292
Andreas Theocharous <atheocharous2@sheffield.ac.uk>
2015-12-13 22:25:28 +0000

    Added the other two types

--- src/src/models/AppointmentType.java
+++ src/src/models/AppointmentType.java
@@ -3,7 +3,9 @@ package models;
 public enum AppointmentType {
     HYGIENE("Hygiene"),
     CHECK_UP("Check Up"),
-    AMALGAM_FILLING("Amalgam Filling");
+    AMALGAM_FILLING("Amalgam Filling"),
+    RESIN_FILLING("Resin Filling"),
+    GOLD_CROWN("Gold Crown Filling");
 
     private String type;
     AppointmentType(String t) { type = t; }
@@ -12,7 +14,9 @@ public enum AppointmentType {
         String[] temp = {
                 HYGIENE.toString(),
                 CHECK_UP.toString(),
-                AMALGAM_FILLING.toString() };
+                AMALGAM_FILLING.toString(),
+                RESIN_FILLING.toString(),
+                GOLD_CROWN.toString() };
         return temp;
     }
 }
